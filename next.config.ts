819245ab69769bc549bd8ede2b60673e0d70b0a2
Anthony Vargas <av491@MacBook-Air-63.local>
2026-05-05 13:15:30 -0700

    fix build for redeployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const repoName = 'nextjs-portfolio';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
